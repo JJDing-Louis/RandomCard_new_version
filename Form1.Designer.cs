822895d1743e3352aeab2384c5b155c805004a4a
J.J.Ding <louis7012161@gmail.com>
2021-04-03 16:49:57 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> New version

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RandomCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtPlayer = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResult
+            // 
+            this.btnResult.Font = new System.Drawing.Font("新細明體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnResult.Location = new System.Drawing.Point(181, 14);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(106, 25);
+            this.btnResult.TabIndex = 0;
+            this.btnResult.Text = "按我一下";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txtPlayer
+            // 
+            this.txtPlayer.Location = new System.Drawing.Point(12, 14);
+            this.txtPlayer.Name = "txtPlayer";
+            this.txtPlayer.Size = new System.Drawing.Size(163, 25);
+            this.txtPlayer.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 323);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 383);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(698, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 491);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPlayer);
+            this.Controls.Add(this.btnResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox txtPlayer;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomCard
+{
+    public partial class Form1 : Form
+    {
+        private int cardNumber = 52;//宣告牌數
+
+        /// <summary>
+        /// 宣告手牌空間
+        /// </summary>
+        private List<int> card = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //以下這一段是dataGridView
+            /* dataGridView1.ColumnCount = 2;
+             dataGridView1.Columns[0].Name = "玩家";
+             dataGridView1.Columns[1].Name = "手牌";*/
+            getCard();
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            //DataGridView內容重置(判斷有無內容)
+
+            if (dataGridView1.Columns.Count != 0)
+            {
+                dataGridView1.Columns.Clear();
+            }
+
+            if (int.TryParse(txtPlayer.Text, out int numPlayer)
+                && numPlayer > 0)
+            {
+                if (numPlayer <= cardNumber)
+                {
+                    Reshuffle();
+                    //dataGridView1.ColumnCount = numPlayer; //欄數為玩家人數
+                    int getCardNumber = card.Count / numPlayer; //每個人分到的牌
+                    //int[,] cardIndex = new int[getCardNumber, numPlayer];//建立陣列
+                    for (int i = 0; i < numPlayer; i++)
+                    {
+                        dataGridView1.Columns.Add(string.Empty, $"玩家{(i + 1)}"); //命名玩家
+                    }
+
+                    //將牌發到陣列中
+                    for (int i = 0; i < getCardNumber; i++)
+                    {
+                        string[] row = new string[numPlayer];
+
+                        for (int j = 0; j < numPlayer; j++)
+                        {
+                            row[j] = card[(j * getCardNumber) + i].ToString();
+                        }
+
+                        dataGridView1.Rows.Add(row);
+                        //for (int j = 0; j < getCardNumber; j++)
+                        //{
+                        //    dataGridView1.Rows.ad
+                        //    cardIndex[j, i] = card[(i * getCardNumber) + j];
+                        //}
+                    }
+
+                    //以下轉成列插入
+                    //for (int i = 0; i < getCardNumber; i++)
+                    //{
+                    //    string[] row = new string[numPlayer];
+                    //    for (int j = 0; j < numPlayer; j++)
+                    //    {
+                    //        row[j] = $"{cardIndex[i, j]}";
+                    //    }
+                    //    dataGridView1.Rows.Add(row);
+                    //}
+
+                    //此區寫剩下的牌數
+                    richTextBox1.Clear();
+                    int cardLeft = card.Count % numPlayer;
+                    if (cardLeft != 0)
+                    {
+                        //20210327
+                        //string txtMessage = string.Empty; //顯示訊息
+
+                        StringBuilder build = new StringBuilder();
+
+                        build.Append($"剩下牌數{cardLeft}\n");
+                        for (int h = numPlayer * getCardNumber; h < cardNumber; h++)
+                        {
+                            build.Append($"{card[h]},");
+                        }
+                        richTextBox1.AppendText(build.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("玩家太多，且牌不夠!!!");
+                };
+            }
+            else
+            {
+                MessageBox.Show("請輸入正確數字");
+            }
+            //清除輸入訊息框
+            txtPlayer.Clear();
+        }
+
+        /// <summary>
+        /// 產生52張牌
+        /// </summary>
+        /// <returns></returns>
+        public void getCard()
+        {
+            //List<int> card = new List<int>();
+            for (int i = 1; i <= cardNumber; i++)
+            {
+                card.Add(i);
+            }
+        }
+
+        /// <summary>
+        /// 以下為洗牌動作
+        /// </summary>
+        public void Reshuffle()
+        {
+            Random rnd = new Random();//亂數
+            //List<int> temp = new List<int>();//建立暫存空間
+            //for (int i = cardNumber; i > 0; i--)
+            //{
+            //    int c = rnd.Next(0, i);
+            //    temp.Add(card[c]);
+            //    card.Remove(card[c]);
+            //}
+            for (int i = 0; i < cardNumber; i++)
+            {
+                int pick_Index = rnd.Next(0, cardNumber);
+                int temp = card[i];
+                card[i] = card[pick_Index];
+                card[pick_Index] = temp;
+            }
+        }
+    }
+}
